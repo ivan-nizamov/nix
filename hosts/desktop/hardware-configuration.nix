ad93c554a8b3bb4fd5e054cf4e8f03723177fdd4
Ivan Nizamov <ivan.nizamov@proton.me>
2025-11-16 13:08:30 +0200

    Copied over new hardware configuration.

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -8,13 +8,13 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/368defc1-7341-46bc-8dfd-bae6c14f003f";
+    { device = "/dev/disk/by-uuid/b5ef6a14-d648-4f11-87c9-9b2644a7bdd1";
       fsType = "ext4";
     };
 
