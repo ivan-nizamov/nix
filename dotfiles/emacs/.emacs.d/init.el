1653840cb15163d75e85f84a3647f1c3392332d7
Ivan Nizamov <ivan.nizamov@proton.me>
2025-12-01 21:38:57 +0200

    Teaked unconsequential settings.

--- dotfiles/emacs/.emacs.d/init.el
+++ dotfiles/emacs/.emacs.d/init.el
@@ -227,3 +227,15 @@
 
 ;; Uncomment to enable auto-tangle:
 (add-hook 'after-save-hook #'my/tangle-on-save)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
