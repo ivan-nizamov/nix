ddeb66716ac8dd42208d7c44d631c8201e602120
Ivan Nizamov <ivan.nizamov@proton.me>
2026-01-25 16:18:40 +0200

    Rename legion host to mainframe

---
+++ hosts/mainframe/configuration.nix
@@ -0,0 +1,360 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  zen-browser,
+  lib,
+  inputs,
+  yandex-browser,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  #Experimental features
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.kernelParams = [
+    "amd_pstate=guided"
+    # Fixes intermittent keyboard failures on Lenovo Legions
+    "i8042.reset"
+    "i8042.nomux"
+    "i8042.nopnp"
+    "i8042.noloop"
+    # Disables USB autosuspend globally
+    # "usbcore.autosuspend=-1"
+    # Fix screen flickering/blackouts on AMD iGPUs (Scatter/Gather display issue)
+    "amdgpu.sg_display=0"
+  ];
+
+  # Enable firmware and microcode updates
+
+  hardware.enableRedistributableFirmware = true;
+
+  boot.kernelModules = ["kvm" "kvm-amd"];
+  boot.kernel.sysctl = {"vm.swappiness" = 1;};
+
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 16 * 1024;
+    }
+  ];
+
+
+  networking.hostName = "mainframe"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Bucharest";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "ro_RO.UTF-8/UTF-8"
+  ];
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us,ru,ro";
+    variant = ",,std";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Virtualization: libvirtd for KVM/QEMU
+  # virtualisation.libvirtd.enable = true;
+
+
+
+  # Enable touchpad support (enabled default in most desktopManager).
+
+  # services.xserver.libinput.enable = true;
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.iva = {
+    isNormalUser = true;
+    description = "IVA";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "input"
+    ];
+    shell = pkgs.zsh;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages =
+    # --- UNSTABLE PACKAGES (Default) ---
+    (with pkgs; [
+      git
+      gh
+      stow
+      emacs
+      vscode
+      zed-editor
+      codex
+      starship
+      zoxide
+      tree
+      bitwarden-desktop
+       maple-mono.NF
+       eb-garamond
+      dconf-editor
+      fastfetch
+      obs-studio
+      bat
+      ripgrep
+      fd
+      gparted
+      libsForQt5.xp-pen-deco-01-v2-driver
+      ghostty
+      nix-search-cli
+      nixd
+      rnote
+      anki-bin
+      # mpv
+      apple-cursor
+      gnome-tweaks
+      nodejs_22
+      wl-clipboard
+      blender
+      davinci-resolve
+      ffmpeg_7-full
+      audacity
+      orca-slicer
+      vcv-rack
+      (pkgs.writeShellScriptBin "davinci-nvidia" ''
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        export QT_QPA_PLATFORM=xcb
+        exec ${pkgs.davinci-resolve}/bin/davinci-resolve "$@"
+      '')
+      (pkgs.writeShellScriptBin "lenovo-conservation" ''
+        if [ "$1" == "1" ]; then
+          echo 1 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode
+        else
+          echo 0 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode
+        fi
+      '')
+      gnomeExtensions.space-bar
+      qbittorrent
+      clock-rs
+      zoom-us
+    ])
+    ++
+    # --- STABLE PACKAGES (NixOS 24.11) ---
+    # Use this for packages that fail to build on unstable (e.g. huge Qt apps)
+    (with inputs.nixpkgs-stable.legacyPackages.${pkgs.stdenv.hostPlatform.system}; [
+      easyeffects
+    ])
+    ++
+    # --- FLAKE INPUTS ---
+    [
+      zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+      inputs.ayugram-desktop.packages.${pkgs.stdenv.hostPlatform.system}.ayugram-desktop
+      yandex-browser.packages.${pkgs.stdenv.hostPlatform.system}.yandex-browser-stable
+      inputs.opencode.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ];
+
+  fonts.fontDir.enable = true;
+  fonts.packages = with pkgs; [
+    eb-garamond
+    maple-mono.NF
+  ];
+
+
+   programs.zsh.enable = true;
+
+
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    openssl
+    zlib
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Disable the conflicting default power manager
+  services.power-profiles-daemon.enable = false;
+
+  # Handle power button press with hybrid-sleep
+  services.logind.settings.Login.HandlePowerKey = "hybrid-sleep";
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      # Disable autosuspend for USB input devices (keyboards/mice)
+      USB_AUTOSUSPEND = 0;
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    # 1 enables conservation mode (60% limit), 0 disables it (100% charge)
+    "w /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode - - - - 1"
+  ];
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Allow 'iva' to run the conservation script without a password
+  security.sudo.extraRules = [
+    {
+      users = ["iva"];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/lenovo-conservation";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
+
+  # Open ports in the firewall.
+  networking.firewall = {
+    allowedUDPPortRanges = [
+      {
+        from = 59000;
+        to = 65000;
+      } # Helps with P2P connections (Telegram)
+    ];
+  };
+
+  # Fix for Screen Sharing on Wayland
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+
+  # Force Wayland for Qt apps (Telegram)
+  environment.sessionVariables = {
+    QT_QPA_PLATFORM = "wayland";
+  };
+
+  # Graphics / Nvidia
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    # Required for DaVinci to see OpenCL/CUDA
+    extraPackages = with pkgs; [
+      rocmPackages.clr
+      rocmPackages.rocminfo
+    ];
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+    prime = {
+      sync.enable = false;
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      amdgpuBusId = "PCI:5:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  specialisation = {
+    performance.configuration = {
+      system.nixos.tags = ["performance"];
+      hardware.nvidia = {
+        powerManagement.finegrained = lib.mkForce false;
+        prime = {
+          sync.enable = lib.mkForce true;
+          offload = {
+            enable = lib.mkForce false;
+            enableOffloadCmd = lib.mkForce false;
+          };
+        };
+      };
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
