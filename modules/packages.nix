dc0cf3ef9484fc1b90184970d51ef00a9f97ed59
Ivan Nizamov <ivan.nizamov@proton.me>
2026-02-04 16:19:14 +0200

    Add gogcli package

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,4 +1,17 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  gogcli = pkgs.buildGoModule rec {
+    pname = "gogcli";
+    version = "0.9.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "steipete";
+      repo = "gogcli";
+      rev = "v${version}";
+      sha256 = "18ycgcax2hkfahlvadnhgk5k3agncqp262qcmsyg1rgz6zk70x0d";
+    };
+    vendorHash = "sha256-nig3GI7eM1XRtIoAh1qH+9PxPPGynl01dCZ2ppyhmzU=";
+    go = pkgs.go_1_25;
+  };
+in {
   environment.systemPackages = with pkgs; [
     git
     gh
@@ -21,6 +34,7 @@
     gparted
     libsForQt5.xp-pen-deco-01-v2-driver
     ghostty
+    gogcli
     nix-search-cli
     nixd
     rnote
